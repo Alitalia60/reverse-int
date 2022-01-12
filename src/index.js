edaf23f0fe4dbbab96b498b82523747e93fee343
Andrew Litvinov <lita.study@bk.ru>
2022-01-12 12:52:51 +0300

    feat: end task reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    // function reverse(n) {
+
+    if (n == NaN) {
+        return NaN
+    }
+    if (n < 0) {
+        n = n * (-1)
+    }
+    n = String(n).split('').sort((a, b) => -1)
+    n = Number(n.join(''))
+    return n
+
 }
+
+// console.log(reverse(-261));
